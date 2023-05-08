abef5a1ee2d16f949c128c7242bad4dfd8610342
pjkfckr <pjkfckr@gmail.com>
2023-05-08 14:11:58 +0900

    add game logic function

--- rusty_engine_project/tutorial/src/main.rs
+++ rusty_engine_project/tutorial/src/main.rs
@@ -17,7 +17,7 @@ impl Default for GameState {
             high_score: 0,
             current_score: 0,
             enemy_labels: Vec::new(),
-            spawn_timer: Timer::from_seconds(1.0, false),
+            spawn_timer: Timer::from_seconds(1.0, false), // 초당 60프레임
         }
     }
 }
@@ -26,5 +26,15 @@ fn main() {
     let mut game = Game::new();
 
     // setup game here
+
+    // 2개 이상의 게임 로직을 가질 수 있습니다.
+    // 로직이 추가되는 순서가 실행되는 순서가 됩니다.
+    // 로직과 로직 사이 통신을 원한다면 GameState를 사용해야 합니다
+    game.add_logic(game_logic);
     game.run(GameState::default());
+}
+
+fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    game_state.current_score += 1;
+    println!("Current score: {}", game_state.current_score);
 }
