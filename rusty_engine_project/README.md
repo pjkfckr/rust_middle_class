aad1fae8a5a373c2af12da98d8efde1ff13c054b
pjkfckr <pjkfckr@gmail.com>
2023-05-08 13:59:40 +0900

    game state init

---
+++ rusty_engine_project/README.md
@@ -0,0 +1,6 @@
+
+# Rusty Engine Project
+
+러스티 엔진이란 [BEVY](https://bevyengine.org/) 을 기반으로 단순화 계층을 래핑하여 만든 오픈 소스 게임 엔진 입니다.
+
+러스티 엔진의 목적은 단순한 Rust Interface 를 제공하는 데 있습니다.
---
+++ rusty_engine_project/tutorial/src/main.rs
@@ -0,0 +1,30 @@
+use rusty_engine::prelude::*;
+
+// 게임용 데이터를 저장할 위치가 필요할텐데, 엔진의 일부는 아니지만,
+// 단일 프레임 이상 에 대한 엑세스가 필요합니다
+// 그곳은 바로 게임 상태 구조체 입니다
+// 자신의 게임 상태에 사용할 구조체를 제공할 수 있죠
+struct GameState {
+    high_score: u32,
+    current_score: u32,
+    enemy_labels: Vec<String>,
+    spawn_timer: Timer,
+}
+
+impl Default for GameState {
+    fn default() -> Self {
+        Self {
+            high_score: 0,
+            current_score: 0,
+            enemy_labels: Vec::new(),
+            spawn_timer: Timer::from_seconds(1.0, false),
+        }
+    }
+}
+
+fn main() {
+    let mut game = Game::new();
+
+    // setup game here
+    game.run(GameState::default);
+}
